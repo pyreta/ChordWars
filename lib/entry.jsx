48814bc79e85766c34296f08b76f6b01d4c77b3d
Louis Abramson <pyreta@gmail.com>
2016-08-27 21:43:49 -0400

    Map non sharp notes without noteup trigger

--- lib/entry.jsx
+++ lib/entry.jsx
@@ -115,7 +115,9 @@ const App = React.createClass({
         let input = WebMidi.inputs[0];
         input.addListener('noteon', "all",
           (e)=>{
-            MethodModule.playKey("D2");
+            MethodModule.playKey(e.note.name+e.note.octave);
+            MethodModule.colorKey(e.note.name+e.note.octave);
+            KeyActions.keyPressed(NoteConstants[e.note.name+e.note.octave].note);
             console.log(e.note);
           }
         );
